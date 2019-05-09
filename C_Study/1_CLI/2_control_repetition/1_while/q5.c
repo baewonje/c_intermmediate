03cf967c3ecf334e497385da67ad052d37a81584
baewonje <xzc0824@gmail.com>
2019-05-10 08:05:05 +0900

    5월 9일 q5 복습

--- C_Study/1_CLI/2_control_repetition/1_while/q5.c
+++ C_Study/1_CLI/2_control_repetition/1_while/q5.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 void main(){
-		int num=0;
+		int num;
 		int num2=0;
-		int result;
-		double num3=0;
+		int num3;
+		int num4=0;
+		double d_num1,d_num2,average;
 
 
 		printf("몇개의 정수를 입력할 겁니까? : ");
 		scanf("%d", &num);
 
-		while(num >= num  ){
-				printf("%d개의 정수를 입력하세요 : %d\n", num, num2);
-				scanf("%d", &num2);
-		num3 = (double)num/(double)num2;
-		printf("%f\n", num3);
-
+		while(num2 < num  ){
+				printf("개의 정수를 입력하세요 :");
+				scanf("%d", &num3);
+				num4= num4+num3;
+				num2++;
 		}
+		d_num1 = num4;
+		d_num2 = num;
+		average = (double)d_num1/(double)d_num2;
+		printf("총합의 평균 : %f\n", average);
+
+		
 
 }
 
